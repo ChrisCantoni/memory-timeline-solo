f2e27c333bd4926229ca638c9739da41286ca732
Chris Cantoni <chris.cantoni@gmail.com>
2024-01-14 08:14:57 -0600

    Menu rework.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,6 +8,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField';
 import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import LogoutIcon from '@mui/icons-material/Logout';
 
 
@@ -16,6 +19,15 @@ function Nav() {
   const user = useSelector((store) => store.user);
   const [toggleSearch, setToggleSearch] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value)
@@ -30,6 +42,9 @@ function Nav() {
 
   return (
     <div className="nav">
+      <Button className='navLink' onClick={() => setToggleSearch(!toggleSearch)}><SearchIcon sx={{color: "#04E2B7"}}/></Button>
+              {toggleSearch ? <><TextField variant='filled' className='searchField' sx={{backgroundColor: 'white'}} color='secondary' size='small' value={searchTerm} onChange={handleSearchChange}/>
+              <Button color='secondary' variant='contained' onClick={handleSubmit}>Submit</Button></> : ''}
       <Link to="/home">
         <h2 className="nav-title">StarGazer</h2>
       </Link>
@@ -46,28 +61,47 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
-            <Button className='navLink' onClick={() => setToggleSearch(!toggleSearch)}><SearchIcon sx={{color: "#04E2B7"}}/></Button>
-              {toggleSearch ? <><TextField variant='filled' className='searchField' sx={{backgroundColor: 'white'}} color='secondary' size='small' value={searchTerm} onChange={handleSearchChange}/>
-              <Button color='secondary' variant='contained' onClick={handleSubmit}>Submit</Button></> : ''}
-            <Link className="navLink" to="/user">
-              <HomeIcon/>
-            </Link>
-
             
+            
+            <div className='menuDiv'>
+              <Button
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}>
+                <MenuIcon sx={{color:"#04E2B7"}}/>
+              </Button>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  'aria-labelledby': 'basic-button',
+                }}>         
+                
+              <Link className="navLink" to="/user">
+                <HomeIcon/>
+              </Link>
 
-            <Link className="navLink" to="/info">
-              <InfoIcon/>
-            </Link>
+              
 
-            <Link className="navLink" to="/timelines">
-              Timelines
-            </Link>
+              <Link className="navLink" to="/info">
+                <InfoIcon/>
+              </Link>
 
-            <Link className="navLink" to="/about">
-              About
-            </Link>
+              <Link className="navLink" to="/timelines">
+                Timelines
+              </Link>
 
-            <LogOutButton className="navLink" />
+              <Link className="navLink" to="/about">
+                About
+              </Link>
+
+              <LogOutButton className="navLink" />
+              </Menu>
+            </div>
           </>
         )}
 
